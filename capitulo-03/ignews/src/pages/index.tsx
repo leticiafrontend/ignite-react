e976f92c1ac2c3ecca942a8b3531086cfc70bebf
leticiafrontend <leticia.alesilva@outlook.com>
2021-09-23 21:27:27 -0300

    adiciona integracao com api stripe

--- capitulo-03/ignews/src/pages/index.tsx
+++ capitulo-03/ignews/src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next';
+import { stripe } from '../services/stripe';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +25,30 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/woman.svg" alt="woman coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Jd0qHGImI44XzXYwhyYPgML');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
