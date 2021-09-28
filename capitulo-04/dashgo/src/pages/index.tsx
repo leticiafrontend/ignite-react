7e779a8c390ea34abe17f8db9a483eaca1c460a1
leticiafrontend <leticia.alesilva@outlook.com>
2021-09-28 12:48:38 -0300

    adiciona pagina de sign in

--- capitulo-04/dashgo/src/pages/index.tsx
+++ capitulo-04/dashgo/src/pages/index.tsx
@@ -1,5 +1,58 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
+
 const Home = () => {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Sign In
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
