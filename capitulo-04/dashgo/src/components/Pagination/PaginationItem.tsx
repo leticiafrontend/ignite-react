97ffa57490af3bf14293a9070d0f74893615c959
leticiafrontend <leticia.alesilva@outlook.com>
2021-09-29 07:38:37 -0300

    componentizaçao do pagination

---
+++ capitulo-04/dashgo/src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@chakra-ui/react'
+
+interface PaginationItemProps {
+  isCurrent?: boolean;
+  number: number;
+}
+
+export const PaginationItem = ({ isCurrent = false, number }: PaginationItemProps) => {
+  return isCurrent ? (
+    <Button
+      size="sm"
+      fontSize="xs"
+      width="4"
+      colorScheme="pink"
+      disabled
+      _disabled={{ bg: 'pink.500', cursor: 'default' }}
+    >
+      {number}
+    </Button>
+  ) : (
+    <Button
+      size="sm"
+      fontSize="xs"
+      width="4"
+      colorScheme="pink"
+      bg="gray.700"
+      _hover={{ bg: 'gray.500' }}
+    >
+      {number}
+    </Button>
+  )
+}
