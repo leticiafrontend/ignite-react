3e3f7a569363056f7b8b7da99f7a48093480a522
leticiafrontend <leticia.alesilva@outlook.com>
2021-10-06 15:01:30 -0300

    adiciona logica para criaçao de usuario

--- capitulo-04/dashgo/src/pages/users/create.tsx
+++ capitulo-04/dashgo/src/pages/users/create.tsx
@@ -12,9 +12,13 @@ import Link from 'next/link';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -43,11 +47,31 @@ export const CreateUser = () => {
     resolver: yupResolver(createUserFormSchema),
   });
 
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      },
+    },
+  );
   const { errors, isLoading } = formState;
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = (
+  const router = useRouter();
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     data,
   ) => {
     console.log(data);
+    await createUser.mutateAsync(data);
+    router.push('/users');
   };
   return (
     <Box>
@@ -71,13 +95,13 @@ export const CreateUser = () => {
               <Input
                 name="name"
                 label="Nome completo"
-                {...register}
+                {...register('name')}
                 error={errors.name}
               />
               <Input
                 name="email"
                 label="E-mail"
-                {...register}
+                {...register('email')}
                 error={errors.email}
               />
             </SimpleGrid>
@@ -85,13 +109,13 @@ export const CreateUser = () => {
               <Input
                 name="password"
                 label="Senha"
-                {...register}
+                {...register('password')}
                 error={errors.password}
               />
               <Input
                 name="password_confirm"
                 label="Confirmação da senha"
-                {...register}
+                {...register('password_confirm')}
                 error={errors.password_confirm}
               />
             </SimpleGrid>
